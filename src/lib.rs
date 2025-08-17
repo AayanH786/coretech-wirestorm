6f863c4b412aeae30f2d04424c2f3ef86a9461a5
Aayan H <hussaayan@gmail.com>
2025-08-17 16:02:53 +0100

    imported rustlings multithread server previously worked on

---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+pub struct ThreadPool {
+    // A vector to hold the workers in the pool
+    workers: Vec<Worker>,
+    // holds the sender end of the channel to send jobs to the workers
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+//the job type will hold the closure that will be recieved by the execute method.
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    // Create a new thread pool with the specified number of threads. 
+    pub fn new(size: usize) -> ThreadPool {
+        //make sure thread pool size is > 0, otherwise panic
+        assert!(size > 0, "Thread pool size must be greater than zero");
+
+        //create the channels that we will use
+        let (sender,receiver) = mpsc::channel();
+
+        //wrap the receiver in a mutex so it can be safely shared between threads, and an Arc so it can be cloned safely. 
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        //create a vector (list) of threads that are mutable (can be changed) and with capacity size.
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            //create some workers and store them in the workers vector
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        // return the created threadpool, which includes workers and the sender (only the value held inside is needed)
+        ThreadPool { workers, sender: Some(sender) }
+    }
+    //this lets me send a task into the threadpool for execution by a thread.
+    pub fn execute<F>(&self, f: F)
+    where
+    //closure can be called at least once (FNOnce), safely transferred across threads (Send), static means it only contains static references
+    //FnOnce represents a closure that takes no parameters. 
+        F: FnOnce() + Send + 'static,
+        {
+            let job = Box::new(f);
+            
+            if let Some(sender) = &self.sender {
+                if let Err(e) = sender.send(job) {
+                    eprintln!("Failed to send job to thread pool: {}", e);
+                }
+            } else {
+                eprintln!("Thread pool has been shut down, cannot send job.");
+            }
+        }
+}
+
+//this allows the thread pool to be dropped
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        //take the sender out of the option, which will close the channel
+        drop(self.sender.take()); 
+        //we want to shutdown all workers, which we do by waiting for the current threads to finish (without errors, unwrap)
+        //then these workers are dropped via the drain feature of vectors, the worker is removed from the pool.
+        for worker in self.workers.drain(..) {
+            println!("Shutting down worker {}", worker.id);
+            if let Err(e) = worker.thread.join() {
+                eprintln!("Worker {} thread failed to join: {:?}", worker.id, e);
+            }
+        }
+    }
+}
+
+//define a new data struture called worker, which represents a single worker in the pool
+struct Worker {
+    //id for debugging purposes
+    id: usize,
+    //holds the thread that will be used
+    thread: thread::JoinHandle<()>,
+}
+
+//worker methods
+impl Worker {
+    //create a new worker that holds a thread, the receiver is held within an arc and a mutex as seen earlier
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        //create a thread using the thread::spawn function
+        let thread = thread::spawn(move || {
+            //loop forever
+            loop {
+                //the receiver passes in a job, this job is locked (to acquire the mutex)
+                //then we unwrap to check if there is an error with acquiring a lock
+                //then we call recv to get the job from the mutex.
+                //recv blocks the thread until a job is available, and mutex ensures only 1 worker tries to request a job.
+                let message = match receiver.lock() {
+                    Ok(guard) => guard.recv(),
+                    Err(e) => {
+                        eprintln!("Worker {id} failed to lock the receiver: {e}");
+                        break;
+                    }
+                };
+
+                //check if we got a job or an error
+                match message {
+                    //if we got a clean job, we execute it
+                    Ok(job) => {
+                        println!("Worker {id} got a job; executing.");
+                        job();
+                    }
+                    Err(_) => {
+                        //if we got an error, we print it
+                        println!("Worker {id} got an error; shutting down.");
+                        break;
+                    }
+                }
+            }
+        });
+        //return the created worker
+        Worker {id, thread}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,91 @@
+//see task 1 for explanations
+use std::{fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, time::Duration};
+//import the threads module
+use std::thread;
+//import the threadpool module from my lib.rs file.
+use coretech_wirestorm::ThreadPool; 
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:33333").unwrap_or_else(|err| {
+        eprintln!("Failed to bind to address: {}", err);
+        std::process::exit(1);
+    });
+    // create a thread pool with 4 threads
+    let pool = ThreadPool::new(4); 
+
+    //listener.incoming returns an iterator over the incoming connections
+    //each stream represents an open connection between the server and a client
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    //handle the connection in a thread from the pool if the stream is valid
+                    pool.execute(|| {
+                        handle_connection(stream);
+                    });
+                }
+                Err(e) => {
+                    eprintln!("Failed to accept connection: {e}");
+                }
+            }
+        }
+}
+
+//this function will handle incoming tcp connections
+fn handle_connection(mut stream: TcpStream) {
+    //create a new buffered reader that reads from the stream (doesnt consume it)
+    let buf_reader = BufReader::new(&stream);
+
+    //read the first line of the http request, and it should contain the request method and path (for us, GET and /)
+    let request_line = match buf_reader.lines().next() {
+        //success, we can unwrap the line
+        Some(Ok(line)) => line,
+        //opened the connection but failed to read the line
+        Some(Err(e)) => {
+            eprintln!("Failed to read from connection {e}");
+            return;
+        }
+        //connection was closed before any data was received
+        None => {
+            eprintln!("Connection Closed before request was received");
+            return;
+        }
+    };
+
+    //check which path the request was for
+
+    let (filename, status_line) = match &request_line[..] {
+        //if root path, send the hello file
+        "GET / HTTP/1.1" => ("hello.html", "HTTP/1.1 200 OK"),
+        //if for the sleep subpath, simulate a long response
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("hello.html", "HTTP/1.1 200 OK")
+        }
+        //if for any other path, return a 404
+        _ => ("404.html", "HTTP/1.1 404 NOT FOUND"),
+
+    };
+    //try to read the webpage that was asked
+    let contents = match fs::read_to_string(filename) {
+        Ok(contents) => contents,
+        Err(e) => {
+            eprintln!("Failed to read {}: {}", filename, e);
+            return;
+        }
+    };
+    let length = contents.len();
+
+    //response will hold a successful message, and the contents of the html page
+    let response = format! (
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+
+    //try send the response, if the error returns true, send an error message
+    if let Err(e) = stream.write_all(response.as_bytes()) {
+        eprintln!("Failed to write to connection: {e}");
+    } else {
+        println!("Response sent successfully");
+    }
+
+
 }
